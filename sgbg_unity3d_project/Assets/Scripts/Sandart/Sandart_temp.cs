e07d8294a97c01d5bfc2be1aac1bec5d1ce0a745
christyyoon <artemisyoon@hanmail.net>
2014-05-15 16:50:42 +0900

    test1

--- sgbg_unity3d_project/Assets/Scripts/Sandart/Sandart_temp.cs
+++ sgbg_unity3d_project/Assets/Scripts/Sandart/Sandart_temp.cs
@@ -5,11 +5,11 @@ public class Sandart_temp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("sandart");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
