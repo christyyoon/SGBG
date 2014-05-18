0f91d856e1522df36c528c926485ecc47a32b6aa
JunchulLee <moto151@naver.com>
2014-05-18 16:18:39 +0900

    Complete delete function regardless of order, and multi deleting is possible

--- sgbg_unity3d_project/Assets/Scripts/Gallery/galleryobj.cs
+++ sgbg_unity3d_project/Assets/Scripts/Gallery/galleryobj.cs
@@ -17,7 +17,43 @@ public class galleryobj : MonoBehaviour {
 		fcount = Directory.GetFiles ("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot", "*", SearchOption.AllDirectories).Length; // Count the number of file(파일개수)
 	
 
+		/* Code about deletion picture randomly */
 
+		// Examine png file's name as fcount ex) fcount = 2, (capture1.png, capture2.png) -> normal (capture1.png, capture3.png) -> abnormal
+		for (int j=1; j<=fcount; j++) 
+		{
+
+			// If png file not exist about correspond number of file
+			if(File.Exists("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + j + ".png")==false)
+			{
+				// Examine next number file as remained file number(fcount-examined file count)
+				for(int k=1; k<=fcount-j+1; k++)
+				{
+					int jk = j+k;
+
+					// If next number's file exist
+					if(File.Exists("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (j+k) + ".png")==true) 
+
+						// Move next number's file to (next-1) number's file
+						File.Move("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (j+k) + ".png", "C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (j+k-1) + ".png");
+
+					// If there not exist next number's file
+					else if(File.Exists("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (j+k) + ".png")==false)
+					{
+
+						// If certain number's file is filled by another number's file name, then stop
+						while(File.Exists("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (j+k-1) + ".png")==false)
+						{
+							jk=jk+1; // Because find nextfile, if not exist then find next file again........
+
+							// If find file, then change file name
+							if(File.Exists("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (jk) + ".png"))
+								File.Move("C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (jk) + ".png", "C:\\Users\\Micky\\Documents\\last\\artbox_Data\\screenshot\\capture" + (j+k-1) + ".png");
+						}
+					}
+				}
+			}
+		}
 
 
 		for (int i=1; i<=fcount; i++) { // Create object number of fcount 
